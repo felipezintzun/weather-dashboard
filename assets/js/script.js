2b3b9d90c28fb1c1dadca2415a4707990f86b338
felipezintzun <felipezintzun@gmail.com>
2022-03-13 16:35:06 -0500

    added get oneCity function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,22 @@ var apiKey = "7a8c1bcf69306c6abefcbac5b212cfd8"
          type:'GET',
          success: function(results) {
              console.log(results)
+             var lat = results.coord.lat
+             var lon = results.coord.lon
+             console.log(lat,lon)
+             oneCity(lat,lon)
          }
      })
  }
+
+ function oneCity (lat,lon) {
+    $.ajax({
+        url:`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`,
+        type:'GET',
+        success: function(results) {
+            console.log(results)
+        }
+    })
+}
+
 
