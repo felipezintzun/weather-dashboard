19c81ae22bfc8127ef18ba01d29a7471d663c298
felipezintzun <felipezintzun@gmail.com>
2022-04-10 17:49:20 -0500

    receiving currnet weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,56 @@
 var apiKey = "7a8c1bcf69306c6abefcbac5b212cfd8"
 
- $("#submit-btn").on("click",function(){
+$("#submit-btn").on("click", function () {
     var cityInput = $("#input-area").val()
     console.log(cityInput)
 
     coordinatesCall(cityInput)
- })
+})
 
- function coordinatesCall (cityInput) {
-     $.ajax({
-         url:`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`,
-         type:'GET',
-         success: function(results) {
-             console.log(results)
-             var lat = results.coord.lat
-             var lon = results.coord.lon
-             console.log(lat,lon)
-             oneCity(lat,lon)
-         }
-     })
- }
+function coordinatesCall(cityInput) {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`,
+        type: 'GET',
+        success: function (results) {
+            console.log(results)
+            var lat = results.coord.lat
+            var lon = results.coord.lon
+            console.log(lat, lon)
+            oneCity(lat, lon)
+        }
+    })
+}
 
- function oneCity (lat,lon) {
+function oneCity(lat, lon) {
     var request;
 
     request = $.ajax({
-        url:`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`,
-        type:'GET',
-        success: function(results) {
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`,
+        type: 'GET',
+        success: function (results) {
             console.log(results)
+
+            $("#current-weather").html(`
+            <div class="card">
+            <div class="card-content">
+                <div class="content">
+                <img src="http://openweathermap.org/img/wn/${results.current.weather[0].icon}@2x.png"/>
+                <p>temp:${results.current.temp}</p>
+                <p>Description: ${results.current.weather[0].main}
+                <p>weather:${results.current.weather}</p>
+                <p>wind_speed:${results.current.wind_speed}</p>
+                <p>humidity:${results.current.humidity}</p>
+                </div>
+            </div>
+            </div>
+            `)
+
         }
     });
 
-    request.done(function(response){
-        formatSearch(response);
-    });
+    // request.done(function(response){
+    //     formatSearch(response);
+    // });
 }
 
 
@@ -45,6 +61,6 @@ function formatSearch(jsonObject) {
 
     $("#city-name").text(city_name);
     $("#city-weather").text(city_weather);
-    $("#city-temp").text(city_temp+ " Fahrenheit");
+    $("#city-temp").text(city_temp + " Fahrenheit");
 
 }
