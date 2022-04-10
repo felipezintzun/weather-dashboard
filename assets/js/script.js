3b906238372901bc9542be8d9c6aadf167e6c93c
felipezintzun <felipezintzun@gmail.com>
2022-04-10 18:43:20 -0500

    added local storage, button is currently appearing after each click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ var apiKey = "7a8c1bcf69306c6abefcbac5b212cfd8"
 $("#submit-btn").on("click", function () {
     var cityInput = $("#input-area").val()
     console.log(cityInput)
-
+        $("#previousCity").text(cityInput)
     coordinatesCall(cityInput)
-})
+});
 
 function coordinatesCall(cityInput) {
     $.ajax({
@@ -17,9 +17,35 @@ function coordinatesCall(cityInput) {
             var lon = results.coord.lon
             console.log(lat, lon)
             oneCity(lat, lon)
+
+            var previousSearch = JSON.parse(localStorage.getItem("weather-dash")) || []
+            previousSearch.push(cityInput)
+            localStorage.setItem("weather-dash",JSON.stringify(previousSearch))
+            searchHistory()
         }
     })
-}
+};
+
+searchHistory()
+
+function searchHistory () {
+    var previousSearch = JSON.parse(localStorage.getItem("weather-dash")) || []
+    var searchC = "<h2 id='previousCity'></h2>"
+    for (let i = 0;i<previousSearch.length;i++) {
+
+        searchC +=`<button class="button">${previousSearch[i]}</button>`
+
+    }
+
+    $("#searchHistory").html(searchC)
+};
+
+$("#searchHistory").on("click","button",function(event){
+    event.preventDefault();
+
+    var city = $(this).text()
+    coordinatesCall(city)
+});
 
 function oneCity(lat, lon) {
     var request;
@@ -31,16 +57,16 @@ function oneCity(lat, lon) {
             console.log(results)
 
             $("#current-weather").html(`
-            <div class="card">
+            <div class="card"><h2>Current Weather</h2>
             <div class="card-content">
                 <div class="content">
                 <img src="http://openweathermap.org/img/wn/${results.current.weather[0].icon}@2x.png"/>
-                <p>temp:${results.current.temp}</p>
+                <p>Temp:${results.current.temp}</p>
                 <p>Description: ${results.current.weather[0].main}
-                <p>weather:${results.current.weather}</p>
-                <p>wind_speed:${results.current.wind_speed}</p>
-                <p>humidity:${results.current.humidity}</p>
-                <p>uvi:${results.current.uvi}</p>
+                <p>Weather:${results.current.weather}</p>
+                <p>Wind Speed:${results.current.wind_speed}</p>
+                <p>Humidity:${results.current.humidity}</p>
+                <p>UVI:${results.current.uvi}</p>
                 </div>
             </div>
             </div>
@@ -51,13 +77,14 @@ function oneCity(lat, lon) {
                 <div class="card">
                 <div class="card-content">
                     <div class="content">
+                    <span id="date">${moment(data.daily[0].dt, "X").format("MM/DD/YYYY")}</span>
                     <img src="http://openweathermap.org/img/wn/${results.current.weather[0].icon}@2x.png"/>
-                    <p>temp:${results.daily[i].temp.day}</p>
+                    <p>Temp:${results.daily[i].temp.day}</p>
                     <p>Description: ${results.daily[i].weather[0].main}
-                    <p>weather:${results.daily[i].weather}</p>
-                    <p>wind_speed:${results.daily[i].wind_speed}</p>
-                    <p>humidity:${results.daily[i].humidity}</p>
-                    <p>uvi:${results.daily[i].uvi}</p>
+                    <p>Weather:${results.daily[i].weather}</p>
+                    <p>Wind Speed:${results.daily[i].wind_speed}</p>
+                    <p>Humidity:${results.daily[i].humidity}</p>
+                    <p>UVI:${results.daily[i].uvi}</p>
                     </div>
                 </div>
                 </div>
@@ -70,8 +97,7 @@ function oneCity(lat, lon) {
     // request.done(function(response){
     //     formatSearch(response);
     // });
-}
-
+};
 
 function formatSearch(jsonObject) {
     var city_name = jsonObject.name;
@@ -82,4 +108,4 @@ function formatSearch(jsonObject) {
     $("#city-weather").text(city_weather);
     $("#city-temp").text(city_temp + " Fahrenheit");
 
-}
+};
